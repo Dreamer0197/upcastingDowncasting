8894ae77d3fe761742c2744abb0dc3bfef3f815f
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-17 20:42:33 +0300

    Finish

--- upcastingDowncasting/main.cpp
+++ upcastingDowncasting/main.cpp
@@ -6,9 +6,87 @@
 //
 
 #include <iostream>
+#include "upcastingDowncasting.h"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+/*    EMPLOYEE           MANAGER
+      name               name
+      surname            surname
+      salary             salary
+      display            display
+                         department
+                         increaseSalary
+ 
+ 
+ */
+
+int main(int argc, const char * argv[]) 
+{
+    /*
+    Employee *emp1,*emp2;
+    Manager mng("Ogun","Birinci",3500,"Software");
+    Employee emp3("Duygu","Keydal",4000);
+    
+    emp1=&mng;  //Upcasting işleminde yalnızca base classtaki üyelere erişilir(Her yönetici bir çalışan olduğundan problemle karşılaşılmaz)
+    emp2=&emp3;
+    emp1->displayInformation();
+    emp2->displayInformation();
+    mng.displayInformation();
+    
+    
+    
+          DOWNCASTİNG
+    cout<<"----------------------------------------"<<endl;
+    Employee emp("Duygu","Keydal",3900);
+    Employee *emp2=&emp;
+    
+    Manager *mng2=(Manager*)(emp1);//Downcasting (her çalışan yönetici olamayacağı için problemlerle karşılaşılabilir)
+    Manager *mng3=(Manager*)(emp2);
+    
+    cout<<mng3->mngDepartment<<endl;
+    
+    
+    
+    cout<<"-------------------------------------------"<<endl;
+    emp1->displayInformation();
+    mng2->displayInformation();
+    
+    
+    Employee emp1("Ogun","Birinci",3500);
+    Manager mng1("Kaan","Sengun",3500,"Software");
+    cout<<"Referance Parameter:"<<endl;
+    print(emp1);
+    print(mng1);
+    
+    cout<<"\n-------------------------------\nPOINTER"<<endl;
+    Employee *emp2=new Employee("Ogun","Birinci",3500);
+    Manager *mng2=new Manager("Kaan","Sengun",3500,"Software");
+    print2(emp2);
+    print2(mng2);
+    
+    cout<<"\n-------------------------------\nValue Parameter"<<endl;
+    print3(emp1);
+    print3(mng1);
+     
+     VIRTUAL DESTRUCTOR
+    Manager *mng=new Manager("Ogun","Birinci",3500,"Software");
+    Employee *emp=mng;
+    Employee *emp2=new Manager("Ogun","Birinci",3500,"Software");
+    
+    delete emp2;
+    delete emp;
+     */
+    Cat *catAnimal=new Cat();
+    Dog *dogAnimal=new Dog();
+    Duck *duckAnimal=new Duck();
+    
+    Animal *animalObject[]={catAnimal,dogAnimal,duckAnimal};
+    
+    for(int i=0;i<3;i++){
+        animalObject[i]->talk();
+    }
+    
+    
+    
+return 0;
 }
--- upcastingDowncasting/upcastingDowncasting.h
+++ upcastingDowncasting/upcastingDowncasting.h
@@ -4,9 +4,80 @@
 //
 //  Created by Kaan Şengün on 17.11.2023.
 //
+#include <iostream>
+using namespace std;
 
-#ifndef upcastingDowncasting_h
-#define upcastingDowncasting_h
 
+class Employee{
+public:
+    string empName,empSurname;
+    double empSalary;
+    Employee(string name,string surname,double salary){
+        empName=name;
+        empSurname=surname;
+        empSalary=salary;
+    }
+    virtual ~Employee(){
+        cout<<"Employee destructor worked."<<endl;
+    }
+    virtual void displayInformation(){ //Sınıflara özgü davranış göstermesini virtual la yapıyoruz
+        cout<<"Name:"<<empName<<endl;
+        cout<<"Surname:"<<empSurname<<endl;
+        cout<<"Salary:"<<empSalary<<endl;
+    }
+    
+};
+class Manager:public Employee{
+public:
+    string mngDepartment;
+    Manager(string name,string surname,double salary, string department):Employee(name,surname,salary){
+        mngDepartment=department;
+    }
+    ~Manager(){
+        cout<<"Manager destructor worked."<<endl;
+    }
+    void displayInformation(){
+        Employee::displayInformation();
+        cout<<"Department:"<<mngDepartment<<endl;
+    }
+    void increaseSalary(){
+        empSalary+=200;
+    }
+};
+//Virtual Function reference ve pointerla kullanılmalı
+//reference
+void print(Employee &employeeType){
+    employeeType.displayInformation();
+}
+//pointer
+void print2(Employee *employeeType){
+    employeeType->displayInformation();
+}
+//value
+void print3(Employee employeeType){
+    employeeType.displayInformation();
+}
 
-#endif /* upcastingDowncasting_h */
+class Animal{
+public:
+    virtual void talk()=0;//Pure Virtual oluşturduk. Pure Virtua sahip sınıflardan nesne oluşturulamaz.
+    
+};
+class Dog:public Animal{
+public:
+    void talk(){
+        cout<<"Hav hav."<<endl;
+    }
+};
+class Cat:public Animal{
+public:
+    void talk(){
+        cout<<"Meov."<<endl;
+    }
+};
+class Duck:public Animal{
+public:
+    void talk(){
+        cout<<"Vak vak."<<endl;
+    }
+};
