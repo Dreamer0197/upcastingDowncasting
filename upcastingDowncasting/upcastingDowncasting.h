2d6b819b11a5de1a0c6c77582a4cfb204e2bc957
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-17 20:42:22 +0300

    Finish

---
+++ upcastingDowncasting/upcastingDowncasting.h
@@ -0,0 +1,12 @@
+//
+//  upcastingDowncasting.h
+//  upcastingDowncasting
+//
+//  Created by Kaan Şengün on 17.11.2023.
+//
+
+#ifndef upcastingDowncasting_h
+#define upcastingDowncasting_h
+
+
+#endif /* upcastingDowncasting_h */
